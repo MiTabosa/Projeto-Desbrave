ad112e2f3532f3d6a5b34dba013b3503e45255fc
lilianMelo <lilianmelo22022005@gmail.com>
2025-03-06 20:24:20 -0300

    Imagens

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,76 +1,25 @@
 import { useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
 import "./Navbar.css";
 import Button from "../Button/Button";
 import logo from "../../assets/logo.png";
-import avatar from "../../assets/avatar.png"; // Imagem padrão
-import Login from "../Login/Login"
-import Cadastro from "../Cadastro/Cadastro"
 
-const Navbar = ({ isLogged, setIsLogged }) => {
-  const navigate = useNavigate();
-  const [showPopup, setShowPopup] = useState(false); // Controla o popup
-  const user = JSON.parse(localStorage.getItem("user")); // Pega o nome do usuário
 
-  const handleLogout = () => {
-    setIsLogged(false); 
-    localStorage.removeItem("user");
-    navigate("/");
-  };
-
-  const togglePopup = () => {
-    setShowPopup(!showPopup);
-  };
-
-  useEffect(() => {
-    // Fecha o popup ao clicar fora dele
-    const handleClickOutside = (event) => {
-      if (showPopup && !event.target.closest(".user-profile")) {
-        setShowPopup(false);
-      }
-    };
-
-    document.addEventListener("mousedown", handleClickOutside);
-
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [showPopup]);
+const Navbar = () => {
+  const navigate = useNavigate(); 
 
   return (
     <nav className="navbar">
       <img className="logo" src={logo} alt="Logo" />
       <ul className="nav-links">
-        <li><a onClick={() => navigate("/")}>Início</a></li>
-        <li><a onClick={() => navigate("/sobre")}>Sobre</a></li>
-        <li><a onClick={() => navigate("/destaque")}>Destaque</a></li>
-        <li><a onClick={() => navigate("/contato")}>Contato</a></li>
-        <li><a onClick={() => navigate("/mapa")}>Mapa</a></li>
+        <li><a href="#" onClick={() => navigate("/")}>Início</a></li>
+        <li><a href="#" onClick={() => navigate("/sobre")}>Sobre</a></li>
+        <li><a href="#" onClick={() => navigate("/destaque")}>Destaque</a></li>
+        <li><a href="#" onClick={() => navigate("/contato")}>Contato</a></li>
+        <li><a href="#" onClick={() => navigate("/mapa")}>Mapa</a></li>
       </ul>
-
       <div className="buttons">
-        {isLogged ? (
-          <div className="user-profile">
-            <span>Olá, {user?.name}!</span>
-            <img
-              src={avatar}
-              alt="Perfil"
-              className="profile-pic"
-              onClick={togglePopup}
-            />
-            {showPopup && (
-              <div className="popup">
-                <button onClick={() => navigate("/perfil")}>Página do Usuário</button>
-                <button onClick={handleLogout}>Sair</button>
-              </div>
-            )}
-          </div>
-        ) : (
-          <>
-            <Button text="Login" color="#0367A5" size="small" onClick={() => navigate("/login")} />
-            <Button text="Cadastre-se" color="#0367A5" size="small" onClick={() => navigate("/cadastro")} />
-          </>
-        )}
+        <Button text="Login" color="#0367A5" size="small" onClick={() => navigate("/login")} />
+        <Button text="Cadastre-se" color="#0367A5"  size="small" onClick={() => navigate("/cadastro")} />
       </div>
     </nav>
   );

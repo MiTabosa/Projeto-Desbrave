9bc7bd431035920a04339383f1792944b359dc41
Thainá Araujo <thainacf24@gmail.com>
2025-03-06 21:11:58 -0300

    adicionando pasta diretrizes

---
+++ src/components/Diretrizes/Diretrizes.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Navbar/Navbar";
+
+function Diretrizes () {
+    return (
+    <div>
+        <Navbar/>
+        <h1>Testando</h1>
+    </div>
+    );
+}
+
+export default Diretrizes
+

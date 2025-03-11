82e2c89f1e096069f2964c63d4ffe1c0ed3ebf10
Milena Tabosa <Milenatabosa19@gmail.com>
2025-03-11 18:51:41 -0300

    Resolvendo conflitos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+<<<<<<< HEAD
 import PaginaInicial from "./pages/Pontos/PaginaInicial";
 import Scanner from "./pages/Pontos/Scanner" 
 import Mapa from './pages/Mapa/Mapa';
@@ -15,6 +16,19 @@ import Forum from "./pages/Forum/Foruns";
 import Curso from "./pages/Curso/Curso"
 import Sobre from "./pages/Sobre/Sobre"
 
+=======
+import Login from "./components/Login/Login";
+import Cadastro from "./components/Cadastro/Cadastro";
+import EsqueceuSenha from "./components/RecuperaçaoSenha/EsqueceuSenha";
+import RecuperarSenha from './components/RecuperaçaoSenha/RecuperarSenha';
+import RedefinirSenha from './components/RecuperaçaoSenha/RedefinirSenha';
+
+
+import PaginaInicial from "./components/Pontos/PaginaInicial";
+import Scanner from "./components/Pontos/Scanner" 
+import Mapa from './pages/Mapa/Mapa'
+import CuponsDashboard from './pages/Cupons/CuponsDashboard';
+>>>>>>> 7a92405 (pages/Cupons/CuponsDashboard.jsx/CuponsDashboard.css)
 
 function App() {
   const [isLogged, setIsLogged] = useState(!!localStorage.getItem("user"));
@@ -24,6 +38,7 @@ function App() {
       <Routes>
         <Route path="/Forum" element={<Forum/>}></Route>
         <Route path="/" element={<Home />} />
+<<<<<<< HEAD
         <Route path="/paginaInicial" element={<PaginaInicial />} />
         <Route path="/mapa" element={<Mapa />} />
         <Route path="/scanner" element={<Scanner />} />
@@ -36,6 +51,21 @@ function App() {
         <Route path="/diretrizes" element={<Diretrizes />} />
         <Route path="/sobre" element={<Sobre />} />
         <Route path="/curso" element={<Curso />} />
+=======
+        <Route path="/foruns" element={<div>Fóruns Page</div>} />
+        <Route path="/cursos" element={<div>Cursos Page</div>} />
+        <Route path="/desafios" element={<div>Desafios Page</div>} />
+        <Route path="/mapa" element={<div>Mapa Page</div>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/esqueceu-senha" element={<EsqueceuSenha />} />
+        <Route path="/recuperar-senha" element={<RecuperarSenha />} />
+        <Route path="/redefinir-senha" element={<RedefinirSenha />} />
+        <Route path="/paginaInicial" element={<PaginaInicial />} />
+        <Route path="/mapa" element={<Mapa />} />
+        <Route path="/scanner" element={<Scanner />} />
+        <Route path="/cupons" element={<CuponsDashboard />} />
+>>>>>>> 7a92405 (pages/Cupons/CuponsDashboard.jsx/CuponsDashboard.css)
       </Routes>
     </Router>
   );

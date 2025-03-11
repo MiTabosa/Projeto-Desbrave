7a92405f03d882a2503f341340519b34f97952c9
Milena Tabosa <Milenatabosa19@gmail.com>
2025-03-11 18:49:21 -0300

    pages/Cupons/CuponsDashboard.jsx/CuponsDashboard.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Login from "./components/Login/Login";
+import Cadastro from "./components/Cadastro/Cadastro";
+import EsqueceuSenha from "./components/RecuperaçaoSenha/EsqueceuSenha";
+import RecuperarSenha from './components/RecuperaçaoSenha/RecuperarSenha';
+import RedefinirSenha from './components/RecuperaçaoSenha/RedefinirSenha';
+
+
+import PaginaInicial from "./components/Pontos/PaginaInicial";
+import Scanner from "./components/Pontos/Scanner" 
+import Mapa from './pages/Mapa/Mapa'
+import CuponsDashboard from './pages/Cupons/CuponsDashboard';
 
 function App() {
   return (
@@ -10,6 +21,15 @@ function App() {
         <Route path="/cursos" element={<div>Cursos Page</div>} />
         <Route path="/desafios" element={<div>Desafios Page</div>} />
         <Route path="/mapa" element={<div>Mapa Page</div>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/esqueceu-senha" element={<EsqueceuSenha />} />
+        <Route path="/recuperar-senha" element={<RecuperarSenha />} />
+        <Route path="/redefinir-senha" element={<RedefinirSenha />} />
+        <Route path="/paginaInicial" element={<PaginaInicial />} />
+        <Route path="/mapa" element={<Mapa />} />
+        <Route path="/scanner" element={<Scanner />} />
+        <Route path="/cupons" element={<CuponsDashboard />} />
       </Routes>
     </Router>
   );

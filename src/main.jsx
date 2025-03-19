5569d2f8c266724d8afc20877567235f9060918a
Lilian Souza de Melo <lilianmelo22022005@gmail.com>
2025-03-18 21:36:46 -0300

    adição do pwa

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import './styles/Global.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import './styles/Global.css';
 
+// Verifica se o service worker está sendo registrado
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registrado com sucesso: ', registration);
+      })
+      .catch((error) => {
+        console.log('Falha ao registrar o Service Worker: ', error);
+      });
+  });
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Desbrave Recife',
+        short_name: 'Desbrave',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#1E88E5',
+        description: 'Descubra pontos turísticos e resgate cupons em Recife!',
+        icons: [
+          {
+            src: '/desbrave-icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/desbrave-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })

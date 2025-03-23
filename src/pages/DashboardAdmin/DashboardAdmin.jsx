fc709b86eece526d725ac7c67f5f4b75536ef224
Lilian Souza de Melo <lilianmelo22022005@gmail.com>
2025-03-22 21:40:34 -0300

    Teste DashboardAdmin

---
+++ src/pages/DashboardAdmin/DashboardAdmin.jsx
@@ -0,0 +1,98 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import "./DashboardAdmin.css";
+import { PiBookOpenTextThin } from "react-icons/pi";
+import { FaComments } from "react-icons/fa";
+import BottomDashboard from "../../components/BottomDashboard/BottomDashboard";
+import SidebarAdmin from "../../components/SidebarAdmin/SidebarAdmin";
+import NavbarDashboard from "../../components/NavbarDashboard/NavbarDashboard";
+import CardPerfil from "../../components/CardPerfil/CardPerfil";
+import elementoVerde from "../../assets/elemento-verde.png"; 
+import Button from "../../components/Button/Button";
+
+const DashboardAdmin = () => {
+  const navigate = useNavigate();
+
+  const [numCursos, setNumCursos] = useState(12);
+  const [numForuns, setNumForuns] = useState(5);
+
+  const [name, setName] = useState("Admin");
+  const [subName, setSubName] = useState("User");
+
+  return (
+    <SidebarAdmin>
+      <BottomDashboard>
+        <NavbarDashboard />
+
+        {/* Seção Topo */}
+        <div className="secao-superior">
+          <div className="esquerda-secao">
+            <div className="cabecalho-painel">
+              <h2 className="titulo-dashboard">Olá, {name} {subName}!</h2>
+              <p className="paragrafo-dashboard">Bem-vinda de volta! 😃</p>
+              <button
+                  className="adm-home-botao"
+                  onClick={() => navigate("/")}
+              >
+              Home
+              </button>
+              <img src={elementoVerde} alt="elemento verde" />
+            </div>
+          </div>
+        </div>
+
+        {/* Painel do Administrador */}
+        <div className="admin-dashboard-container">
+          <h2 className="admin-title">Painel do Administrador</h2>
+
+          <div className="admin-info-cards">
+            <div className="admin-card admin-card-cursos">
+              <div className="admin-card-icon">
+                <PiBookOpenTextThin size={35} color="#0A5FA3" />
+              </div>
+              <div className="admin-card-text">
+                <p className="admin-card-number">{numCursos}</p>
+                <p>Cursos cadastrados</p>
+              </div>
+            </div>
+
+            <div className="admin-card admin-card-foruns">
+              <div className="admin-card-icon">
+                <FaComments size={30} color="#0A5FA3" />
+              </div>
+              <div className="admin-card-text">
+                <p className="admin-card-number">{numForuns}</p>
+                <p>Fóruns cadastrados</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="admin-buttons">
+            <Button
+              text="Cadastrar Curso"
+              color="#0367A5"
+              size="medium"
+              onClick={() => navigate("/gestaoCursos")}
+            />
+            <Button
+              text="Cadastrar Fórum"
+              color="#0A5FA3"
+              size="medium"
+              onClick={() => navigate("/gestaoForum")}
+            />
+          </div>
+        </div>
+
+        {/* Card de Perfil */}
+        <div className="menu-lateral-admin">
+        <CardPerfil
+          name={name}
+          subName={subName}
+        />
+        </div>
+      </BottomDashboard>
+    </SidebarAdmin>
+  );
+};
+
+export default DashboardAdmin;

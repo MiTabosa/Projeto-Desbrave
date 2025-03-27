56df68a9405275a18cab4d64dd526192af1e45c8
Lilian Souza de Melo <lilianmelo22022005@gmail.com>
2025-03-26 22:38:33 -0300

    Responsividade tela Adm

--- src/components/SidebarAdmin/SidebarAdmin.jsx
+++ src/components/SidebarAdmin/SidebarAdmin.jsx
@@ -1,10 +1,63 @@
 import "./SidebarAdmin.css";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { MdOutlineDashboard } from "react-icons/md";
+import { PiBookOpenTextThin } from "react-icons/pi";
+import { FaAward, FaComments } from "react-icons/fa";
+import { GrTicket } from "react-icons/gr";
+import logoSimplicada from "../../assets/logo-simplificada.png";
 
 export default function SidebarAdmin({ children }) {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div className="dashboard-container">
+      {!isMobile && (
+        <aside className="sidebar-admin">
+          <div className="conteiner-logoAdmin" onClick={() => navigate("/")}>
+            <img className="logo-simplificadaAdmin" src={logoSimplicada} alt="Logo" />
+          </div>
+          <ul>
+            <li
+              className={`sidebar-item ${
+                location.pathname === "/dashboardAdmin" ? "sidebar-item-active" : ""
+              }`}
+              onClick={() => navigate("/dashboardAdmin")}
+            >
+              <MdOutlineDashboard className="icon" />
+              <span>Dashboard</span>
+            </li>
+          </ul>
+        </aside>
+      )}
       
       <div className="dashboard-content">{children}</div>
+      
+      {isMobile && (
+        <aside className="mobile-sidebar">
+          <ul>
+            <li
+              className={`sidebar-item ${
+                location.pathname === "/dashboardAdmin" ? "sidebar-item-active" : ""
+              }`}
+              onClick={() => navigate("/dashboardAdmin")}
+            >
+              <MdOutlineDashboard className="icon" />
+              <span>Dashboard</span>
+            </li>
+          </ul>
+        </aside>
+      )}
     </div>
   );
 }
--- src/pages/DashboardAdmin/DashboardAdmin.jsx
+++ src/pages/DashboardAdmin/DashboardAdmin.jsx
@@ -1,5 +1,5 @@
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
 import "./DashboardAdmin.css";
 import { PiBookOpenTextThin } from "react-icons/pi";
 import { FaComments } from "react-icons/fa";
@@ -12,85 +12,89 @@ import Button from "../../components/Button/Button";
 
 const DashboardAdmin = () => {
   const navigate = useNavigate();
-
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
   const [numCursos, setNumCursos] = useState(12);
   const [numForuns, setNumForuns] = useState(5);
-
   const [name, setName] = useState("Admin");
   const [subName, setSubName] = useState("User");
 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-        <SidebarAdmin>
-    <BottomDashboard>
+    <SidebarAdmin>
+      <BottomDashboard>
         <NavbarDashboard/>
-
-        {/* Seção Topo */}
-        <div className="secao-superior">
-          <div className="esquerda-secao">
-            <div className="cabecalho-painel">
-              <h2 className="titulo-dashboard">Olá, {name} {subName}!</h2>
-              <p className="paragrafo-dashboard">Bem-vinda de volta! 😃</p>
-              <button
-                  className="adm-home-botao"
-                  onClick={() => navigate("/")}
-              >
-              Home
-              </button>
-              <img src={elementoVerde} alt="elemento verde" />
+        {!isMobile && (
+          <div className="secao-superior">
+            <div className="esquerda-secao">
+              <div className="cabecalho-painel">
+                <h2 className="titulo-dashboard">Olá, {name} {subName}!</h2>
+                <p className="paragrafo-dashboard">Bem-vinda de volta! 😃</p>
+                <button
+                    className="adm-home-botao"
+                    onClick={() => navigate("/")}
+                >
+                  Home
+                </button>
+                <img src={elementoVerde} alt="elemento verde" />
+              </div>
             </div>
+            <CardPerfil
+              name={name}
+              subName={subName}
+            />
           </div>
-              {/* Card de Perfil */}
-          {/* <div className="menu-lateral-admin"> */}
-          <CardPerfil
-          name={name}
-          subName={subName}
-        />
-        {/* </div> */}
-        </div>
-
-        {/* Painel do Administrador */}
+        )}
         <div className="admin-dashboard-container">
           <h2 className="admin-title">Painel do Administrador</h2>
 
           <div className="admin-info-cards">
             <div className="admin-card admin-card-cursos">
-              <div className="admin-card-icon">
-                <PiBookOpenTextThin size={35} color="#0A5FA3" />
+              <div className="admin-card-content">
+                <div className="admin-card-icon">
+                  <PiBookOpenTextThin size={35} color="#0A5FA3" />
+                </div>
+                <div className="admin-card-text">
+                  <p className="admin-card-number">{numCursos}</p>
+                  <p>Cursos cadastrados</p>
+                </div>
               </div>
-              <div className="admin-card-text">
-                <p className="admin-card-number">{numCursos}</p>
-                <p>Cursos cadastrados</p>
+              <div className="admin-buttons">
+                <Button
+                  text="Cadastrar Curso"
+                  color="#0367A5"
+                  size={isMobile ? "small" : "medium"}
+                  onClick={() => navigate("/gestaoCursos")}
+                />
               </div>
             </div>
-
             <div className="admin-card admin-card-foruns">
-              <div className="admin-card-icon">
-                <FaComments size={30} color="#0A5FA3" />
+              <div className="admin-card-content">
+                <div className="admin-card-icon">
+                  <FaComments size={30} color="#0A5FA3" />
+                </div>
+                <div className="admin-card-text">
+                  <p className="admin-card-number">{numForuns}</p>
+                  <p>Fóruns cadastrados</p>
+                </div>
               </div>
-              <div className="admin-card-text">
-                <p className="admin-card-number">{numForuns}</p>
-                <p>Fóruns cadastrados</p>
+              <div className="admin-buttons">
+                <Button
+                  text="Cadastrar Fórum"
+                  color="#0A5FA3"
+                  size={isMobile ? "small" : "medium"}
+                  onClick={() => navigate("/gestaoForum")}
+                />
               </div>
             </div>
-          </div>
-
-          <div className="admin-buttons">
-            <Button
-              text="Cadastrar Curso"
-              color="#0367A5"
-              size="medium"
-              onClick={() => navigate("/gestaoCursos")}
-            />
-            <Button
-              text="Cadastrar Fórum"
-              color="#0A5FA3"
-              size="medium"
-              onClick={() => navigate("/gestaoForum")}
-            />
-      
           </div>
         </div>
-
       </BottomDashboard>
     </SidebarAdmin>
   );

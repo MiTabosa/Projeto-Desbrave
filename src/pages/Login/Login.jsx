825070526e3e660b411a0b9e6d0c763dfee4562f
Milena Tabosa <Milenatabosa19@gmail.com>
2025-04-02 12:41:40 -0300

    ajustes na tela de login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,10 @@
 import "./Login.css";
-import { FaGooglePlus } from "react-icons/fa";
 import { FaUser, FaLock } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { IoEyeOff, IoEye } from "react-icons/io5";
 import img_conecta_recife from "../../assets/img_conecta_recife.png";
-import gov_br from "../../assets/gov_br.png";
+
 
 export default function Login() {
   const [showPassword, setShowPassword] = useState(false);
@@ -21,14 +20,7 @@ export default function Login() {
   return (
     <div className="loginContainer">
       <div className="loginEsquerda">
-        <h1>Olá!</h1>
-        <p>
-          Insira algumas informações e <br />
-          comece a sua jornada conosco!
-        </p>
-        <Link to="/Cadastro">
-          <button className="criarConta">Quero criar uma conta</button>
-        </Link>
+
       </div>
 
       <div className="loginDireita">
@@ -56,13 +48,9 @@ export default function Login() {
           <Link to="/esqueceuSenha" className="EsqueciSenha">Esqueceu sua senha?</Link>
           <button className="EntrarBotao" onClick={() => handleLogin()}>Entrar</button>
         </div>
-
-        <p className="connecText">Ou conecte-se com:</p>
-        <div className="socialIcons">
-          <img src= {img_conecta_recife} alt="Conecta Recife" className="socialImg"/>
-          <img src= {gov_br} alt="GOVBr" className="socialImg2"/>
-          <FaGooglePlus className="GoogleIcon"/>
-        </div>
+        
+        <a href="/cadastro" className="BotaoCadastrar">Cadastra-se</a>
+    
         <div>
         <a href="/" className="voltarinicio">↩ Voltar para o inicio</a>
         </div>
